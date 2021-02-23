dc23e395eb52d92023f220dab21aa85cec656daf
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-02-22 23:44:00 -0800

    added chunker and dropper functions, consolidated sql notebook, strated dropping features

--- notebooks/exploratory/functions.py
+++ notebooks/exploratory/functions.py
@@ -10,9 +10,27 @@ def lookup(df, lu_type, lu_item=None):
     
 def col_stripper(df, column):
     "Returns column with leading and trailing whitespaces removed."
-    
     return df[column].apply(lambda x: x.strip())
 
 def fetch(cur, q):
     """Returns an SQL query."""
     return cur.execute(q).fetchall()
+
+def chunker(df, li):
+    """Returns data frame slice with selected columns"""
+    return df[li].head(3)
+
+def dropper(df, li, inplace=None):
+    """Return data frame with select columns dropped
+    
+    Agrs:
+        df: A data frame.
+        li: A list of columns to be dropped.
+        inplace: A Boolean.
+    Returns:
+        A data frame with the selected columns dropped."""
+    
+    if inplace:
+        return df.drop(li, axis=1, inplace=True)
+    else:
+        return df.drop(li, axis=1)
