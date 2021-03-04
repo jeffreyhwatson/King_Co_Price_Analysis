e51701800bb772cc4ff1805e7bad12e02b70e712
Hanif Watson <jeffrey.h.watson@protonmail.com>
2021-03-04 00:16:11 -0800

    added tukey test, forward selection model, and t-tests for recs.

--- notebooks/exploratory/functions.py
+++ notebooks/exploratory/functions.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import statsmodels.api as sm
+import statsmodels.formula.api as smf
 from statsmodels.formula.api import ols
 from statsmodels.stats.diagnostic import linear_rainbow, het_breuschpagan
 from statsmodels.stats.outliers_influence import variance_inflation_factor
@@ -186,5 +187,48 @@ def one_hot(df, feature):
     transformed = ohcoder.transform(df[[feature]])
     return ohcoder, transformed
 
+#_________________________________________________________________________________
+# Title: forward_selected source code
+# Author: Schumacher, A & Smith, T
+# Date: Thursday April 23, 2015
+# Code Version: 1.0
+# Availability: https://planspace.org/20150423-forward_selection_with_statsmodels/
+#_________________________________________________________________________________
+def forward_selected(data, response):
+    """Linear model designed by forward selection.
 
+    Parameters:
+    -----------
+    data : pandas DataFrame with all possible predictors and response
+
+    response: string, name of response column in data
+
+    Returns:
+    --------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by forward selection
+           evaluated by adjusted R-squared
+    """
+    remaining = set(data.columns)
+    remaining.remove(response)
+    selected = []
+    current_score, best_new_score = 0.0, 0.0
+    while remaining and current_score == best_new_score:
+        scores_with_candidates = []
+        for candidate in remaining:
+            formula = "{} ~ {} + 1".format(response,
+                                           ' + '.join(selected + [candidate]))
+            score = smf.ols(formula, data).fit().rsquared_adj
+            scores_with_candidates.append((score, candidate))
+        scores_with_candidates.sort()
+        best_new_score, best_candidate = scores_with_candidates.pop()
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            selected.append(best_candidate)
+            current_score = best_new_score
+    formula = "{} ~ {} + 1".format(response,
+                                   ' + '.join(selected))
+    model = smf.ols(formula, data).fit()
+    return model
     
